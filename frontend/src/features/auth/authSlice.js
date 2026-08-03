7a2db95011b95f663592fd962b3c36ba31812880
Sahel <sahelmhd3@gmail.com>
2026-08-03 11:53:24 +0530

    feat: setup redux toolkit

---
+++ frontend/src/features/auth/authSlice.js
@@ -0,0 +1,34 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  user: null,
+  accessToken: null,
+  refreshToken: null,
+  isAuthenticated: false,
+};
+
+const authSlice = createSlice({
+  name: "auth",
+
+  initialState,
+
+  reducers: {
+    loginSuccess: (state, action) => {
+      state.user = action.payload.user;
+      state.accessToken = action.payload.access;
+      state.refreshToken = action.payload.refresh;
+      state.isAuthenticated = true;
+    },
+
+    logout: (state) => {
+      state.user = null;
+      state.accessToken = null;
+      state.refreshToken = null;
+      state.isAuthenticated = false;
+    },
+  },
+});
+
+export const { loginSuccess, logout } = authSlice.actions;
+
+export default authSlice.reducer;
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,11 +1,17 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import ReactDOM from 'react-dom/client'
+
 import App from './App.jsx'
+import { store } from './app/store.js'
+
 import 'bootstrap/dist/css/bootstrap.min.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  <React.StrictMode>
+    <Provider store = {store}>
+      <App />
+    </Provider>
+    
+  </React.StrictMode>,
 )
